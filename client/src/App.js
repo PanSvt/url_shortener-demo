f699347e3e8c7fa0a059f06d9a73840669c1b115
Pun Svetachinta <pun_svt@Puns-MacBook-Air.local>
2023-01-06 16:35:02 +0700

    feat: add BackgroundAnimate

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import InputLink from "./components/InputLink";
+import BackgroundAnimate from "./utils/BackgroundAnimate";
 
 function App() {
   return (
     <div className="container">
       <InputLink />
+      <BackgroundAnimate />
     </div>
   );
 }
